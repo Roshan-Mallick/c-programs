55c4bd79778465d8fdd27c2eb91798faebe10f0f
Roshan Mallick <roshanmallick2025@gmail.com>
2026-04-16 16:25:12 +0530

    feat : added c program to find largest number

---
+++ easy/largest_of_two_number.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+      int a , b ;
+      printf("Enter two integers: ");
+      scanf("%d %d",&a,&b);
+      a>b?printf("a is greater"):printf("b is greater");
+    return 0 ;
+}
