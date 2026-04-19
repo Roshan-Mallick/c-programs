0c695f248865417ffe7ba8dc02722e8246e225c8
Roshan Mallick <roshanmallick2025@gmail.com>
2026-04-20 00:46:42 +0530

    feat : optimized perfect number checker in C using O(sqrt(n)) approach

---
+++ medium/perfect_number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+
+int main () {
+
+    int n, i, sum = 0;
+
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    for (i = 1 ; i <= n / 2; i++) {
+        if (n % 1 == 0){
+            sum = sum + i;
+        } else {
+            sum = i + (n/i);
+        }
+    }
+
+    if (sum == n && n != 0) {
+        printf("%d is a perfect number \n",n);
+    } else {
+        printf("%d is not a perfect number \n",n);
+    }
+
+
+    return 0;
+}
